9187daf6b7e1d210b2745cc74eea7115b774580e
garrethblouws <garrethblouws@gmail.com>
2025-01-16 17:59:17 +0200

    Delete Python Development directory

--- Python Development/basics/ecommerce/files.py
+++
@@ -1,6 +0,0 @@
-import openpyxl as xl
-
-wb = xl.load_workbook('transactions.xlsx')
-sheet = wb['Sheet1']
-sheet.cell(1,1)
-print(sheet.max_row)
--- Python Development/readfile.py
+++
@@ -1,8 +0,0 @@
-
-orderno_file = open('orderno.txt','r')
-
-orderno = orderno_file.read()
-
-print(orderno)
-
-orderno_file.close()
